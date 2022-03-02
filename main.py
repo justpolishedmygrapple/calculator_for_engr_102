abed9bf7b79a50751a5a32aa129c25c6888a09bd
justpolishedmygrapple <mangela@oregonstate.edu>
2022-03-01 21:33:51 -0800

    Simple calculator for engr_102

--- main.py
+++ main.py
@@ -11,19 +11,20 @@ def validate_num():
         print("Enter a valid number you silly gööse")
         return validate_num()
 def validate_opcode():
-    opcode = input("Enter opcode fron 0-3: ")
+    opcode = input("Enter opcode from 1-5: ")
     try:
         z = int(opcode)
         int(opcode)
         if not int(opcode):
             return validate_opcode()
-        if z == 0 or z == 1 or z == 2 or z == 3:
-            return int(opcode)
+        if z == 1 or z == 2 or z == 3 or z == 4 or z == 5:
+            opcode = int(opcode)
+            return opcode
         else:
             print("enter a valid opcode you silly gööse")
             return validate_opcode()
     except ValueError:
-        print("enter a valid opcode")
+        print("enter a valid opcode you silly gööse")
         return validate_opcode()
 def validate_wyl():
     would_you_like = input("Would you like to use the calculator again: ")
@@ -35,19 +36,21 @@ def validate_wyl():
     else:
         print("enter yes or no you silly gööse")
         return validate_wyl()
-print("opcodes: 0 is addition, 1 is subtraction, 2 is multiplication, 3 is division\n")
+print("opcodes: 1 is addition, 2 is subtraction, 3 is multiplication, 4 is division, 5 is exponentiation\n")
 would_you_like = "yes"
 while would_you_like == "yes":
     first_num = validate_num()
     print("enter another number")
     second_num = validate_num()
     opcode = validate_opcode()
-    if opcode == 0:
-        print(first_num + second_num)
     if opcode == 1:
-        print(first_num - second_num)
+        print(first_num + second_num)
     if opcode == 2:
-        print(first_num * second_num)
+        print(first_num - second_num)
     if opcode == 3:
+        print(first_num * second_num)
+    if opcode == 4:
         print(first_num / second_num)
+    if opcode == 5:
+        print(first_num ** second_num)
     would_you_like = validate_wyl()
